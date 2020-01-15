a0e1ecd66c27136c96d9878f3d98f696a25c0907
Lucas SM <55751877+sm49697@users.noreply.github.com>
2020-01-15 17:07:51 +0800

    controller input

--- bruh.ino
+++ bruh.ino
@@ -1,109 +1,113 @@
-// MACROS
-#define fori(i, n) for(int i = 0; i < n; ++i)                                         // for loop
-#define foris(i, a, n) for(int i = a; i < n; ++i)                                     // for loop, specific
-#define IN(i, l, r) (l < i && i < r)                                                  // check if the value is within the given range (exclusive)
-#define LINR(i, l, r) (l <= i && i <= r)                                              // check if the value is within the given range (inclusive)
-#define elif else if                                                                  // else if but in python
+/**
+ * TODO: Map XBOX triggers to motors
+ * TODO: Map XBOX joysticks to servos
+ * TODO: Map horizontal movement of joysticks to act as modifier for strength of motors
+ */
 
-// IMPORTS
+#include <XBOXONE.h>
 #include <Servo.h>
-#include <Wire.h>
 
-// USER CONFIG:
-int percent = 0;                                                                      // between -100 and 100, indicates how fast the motor 
-                                                                                      // will be moving when the arduino boots
-int pins[] = {9, 10, 11, 3, 5, 6, 100};                                               // the signal output pins (as many as you'd like)
-int servopin = 9;                                                                     // the pin of the servo
-int percentages[] = {50, 50, 50, 50, 50, 50, 50};                                     // percentages for each pin (which are the same)
-                                                                                      // each pin is paired to a percentage
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#include <SPI.h>
+#endif
 
+USB Usb;
+XBOXONE Xbox(&Usb);
 
-const int arraySize = sizeof(pins)/sizeof(byte);                                      // the amount of pins
-Servo controllers[arraySize];                                                         // create an array of servos
-const int pin = 6;
+//Revision 1.3 (DEV-09947)
+#define MAX_RESET 7 //MAX3421E pin 12
+#define MAX_GPX   8 //MAX3421E pin 17
+
+#define ESCPIN 6
+#define WPPIN 5
+
 Servo ESC;
+Servo WP;
 
-void setup() {
-  initializeProgram();                                                                // initializes program -- prints
-  attachPins();                                                                       // attaches all the pins
-  setPwmFrequency(pin, 64); // force pwm at 500Hz
-  ESC.attach(pin,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+void setup()
+{ 
+  ESC.attach(ESCPIN);
+  WP.attach(WPPIN);
+  pinMode(MAX_GPX, INPUT);
+  pinMode(MAX_RESET, OUTPUT); 
+  digitalWrite(MAX_RESET, LOW); 
+  delay(20); //wait 20ms 
+  digitalWrite(MAX_RESET, HIGH);
+  delay(20); //wait 20ms
+  Serial.begin(115200);
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy a/nd other boards with built-in USB CDC serial connection
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); //halt
+  }
+  Serial.print(F("\r\nXBOX USB Library Started"));
+  setPwmFrequency(ESCPIN, 64); // force pwm at 500Hz
+  setPwmFrequency(WPPIN, 64);
+  ESC.attach(ESCPIN,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
   delay(200); // wait
   ESC.writeMicroseconds(1800); // throttle init
   delay(200); // wait
   ESC.writeMicroseconds(1500); // stop
-  delay(1000);                                                                        // delay for 1 second
-  Serial.println("INPUT PERCENTAGE");                                                 // request user input without receiving user input yet
-  pinOtherArduino();                                                                  // pins the slave arduino
+  Serial.println("ESC init done");
+  WP.attach(WPPIN,900,2100);
+  WP.writeMicroseconds(1500);
+  delay(200);
+  WP.writeMicroseconds(1500);
+  Serial.println("WP init done");
+  Serial.println("starting 7.5s stop test");
+  delay(7500);
+  Serial.println("program commencing");
 }
-
-
 void loop() {
-  writeMicrocontrollers();                                                            // write all the microcontrollers with their given values
-  if (Serial.available() > 1) {                                                       // if the console is available to receive input
-    int controlValue = Serial.read() - '0';                                           // read
-    int inputValue = Serial.parseInt();                                               // get the input value for the strength of motors
-    int blank = Serial.read();
-    Serial.print("RECEIVED INSTRUCTIONS FOR ");                                       // print the values given
-    Serial.println(controlValue);
-    Serial.println(inputValue);
-    
-    if (inputValue == 50) {                                                           // if the input value is 50%, stop
-      Serial.println("STOPPING");
-      percentages[controlValue] = 50;
-    } 
-    elif (LINR(inputValue, 0, 100)) {                                                 // else if the input value is within 0 and 100 (inclusive), write the control values
-      setControlValues(controlValue, inputValue);
-    }
-    else Serial.println("INVALID VALUE");                                             // else then the value is not within 0 and 100 and is therefore invalid, tell the user
-    if(LINR(inputValue, 0, 100) && controlValue == 6) {                               // write the wires
-      writeWires();
-    }
+  Usb.Task();
+  if (Xbox.XboxOneConnected) {
+     if (Xbox.getAnalogHat(LeftHatY) > 4000 || Xbox.getAnalogHat(LeftHatY) < -4000) {
+        Serial.print(F("LeftHatY: "));
+        Serial.print(Xbox.getAnalogHat(LeftHatY));
+        double lhy = Xbox.getAnalogHat(LeftHatY);
+        if (lhy > 0){
+          lhy -= 3999;
+        } else {
+          lhy += 3999;
+        }
+        double y = (double) ((double) 1160 / (double) 57536);
+        double b = (double) (lhy * y);
+        b += 1500;
+        Serial.print(", output: ");
+        Serial.print(b);
+        WP.writeMicroseconds(b);
+      }else {
+        Serial.print("no LeftHatY, resetting to 1520   ");
+        WP.writeMicroseconds(1500);
+      }
+      Serial.print("       ");
+      if (Xbox.getAnalogHat(RightHatY)> 4000 || Xbox.getAnalogHat(RightHatY) < -4000) {
+        Serial.print(F("RightHatY: "));
+        Serial.print(Xbox.getAnalogHat(RightHatY));
+        double rhy = Xbox.getAnalogHat(RightHatY);
+        if (rhy > 0){
+          rhy -= 3999;
+        } else {
+          rhy += 3999;
+        }
+        double t = (double) ((double) 1000 / (double) 57536);
+        double v = (double) (rhy * t);
+        v += 1500;
+        Serial.print(", output: ");
+        Serial.print(v);
+        ESC.writeMicroseconds(v);
+      }
+      else {
+        Serial.print("no RightHatY, resetting to 1500   ");
+        ESC.writeMicroseconds(1500);
+      }
+      Serial.println();
+    //}
   }
-}
-
-void initializeProgram() {                                                            // initializes the wire and serials, tells the user
-  Wire.begin();
-  Serial.begin(9600);
-  Serial.println("PROGRAM INITIALIZED");
-  Serial.println(arraySize);
-}
-
-void attachPins() {                                                                   // attaches all the pins
-  fori(i, arraySize) {
-    Serial.print("attached ");
-    Serial.print(i);
-    Serial.print(" to ");
-    Serial.println(pins[i]);
-    controllers.attach(pins[i]);
-  }
-  cservo.attach(servopin);
-}
-
-void writeMicrocontrollers() {                                                        // write all the controller's seconds 
-  fori(i, arraySize) {
-    if(pins[i] < 7) controllers.writeMicroseconds(percentages[i] * 14 + 800); 
-    else controllers[i].writeMicroseconds(percentages[i] * 10 + 1000);
-  }
-}
-
-void pinOtherArduino() {                                                              // pins the slave arduino
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-}
-
-void setControlValues(int cv, int iv) {                                               // sets the percentage values for the controllers
-  percentages[cv] = iv;
-  Serial.print("SET VALUE TO: ");
-  Serial.print(percentages[cv]);
-  Serial.println("%");
-}
-
-void writeWires() {                                                                   // write the wires
-  Wire.beginTransmission(8);
-  Wire.write((byte)floor((percentages[6] * 10 + 1000) / 64));
-  Wire.write((percentages[6] * 10 + 1000) - ((byte) floor((percentages[6] * 10 + 1000)/64)*64));
-  Wire.endTransmission();
+  
+  delay(12);
 }
 
 void setPwmFrequency(int pin, int divisor) {
@@ -117,10 +121,10 @@ void setPwmFrequency(int pin, int divisor) {
       case 1024: mode = 0x05; break;
       default: return;
     }
-    if(pin == 5 || pin == 6) {
-      TCCR0B = TCCR0B & 0b11111000 | mode;
+    if(pin == 6) {
+      TCCR3B = TCCR3B & 0b11111000 | mode;
     } else {
-      TCCR1B = TCCR1B & 0b11111000 | mode;
+      TCCR4B = TCCR4B & 0b11111000 | mode;
     }
   } else if(pin == 3 || pin == 11) {
     switch(divisor) {
