5b044e81f621cfd95abbe5445a4d3417708c79bf
sm49697 <stamaria49697@sas.edu.sg>
2020-01-31 11:56:12 +0800

    accelerometer functions verified, now print. TODO: debug gyro

--- bruh/bruh.ino
+++ bruh/bruh.ino
@@ -68,6 +68,7 @@ void setup() {
   setPwmFrequency(LEFTMOTORPIN, 64); // force pwm at 500Hz
   setPwmFrequency(LEFTSERVOPIN, 64);
   initGyro();
+  initAccelerometer();
   secondAttachAndPin();
   stopTest();
 }
@@ -135,7 +136,7 @@ void secondAttachAndPin() {
   LEFTSERVO.writeMicroseconds(1500);
   delay(200);
   LEFTSERVO.writeMicroseconds(1500);
-  Serial.println("LEFTESC init done");
+  Serial.println("LEFTSERVO init done");
 }
 
 void initGyro() {
@@ -143,6 +144,58 @@ void initGyro() {
   Serial.println("Calibrating Gyro");
   gyro.zeroCalibrate(200, 10);
   Serial.println("Gyro calibrated");
+}
+
+void initAccelerometer() {
+  Serial.println("Initializing accelerometer");
+  
+  accel.powerOn();
+  //set activity/ inactivity thresholds (0-255)
+  accel.setActivityThreshold(75); //62.5mg per increment
+  accel.setInactivityThreshold(75); //62.5mg per increment
+  accel.setTimeInactivity(10); // how many seconds of no activity is inactive?
+ 
+  //look of activity movement on this axes - 1 == on; 0 == off 
+  accel.setActivityX(1);
+  accel.setActivityY(1);
+  accel.setActivityZ(1);
+ 
+  //look of inactivity movement on this axes - 1 == on; 0 == off
+  accel.setInactivityX(1);
+  accel.setInactivityY(1);
+  accel.setInactivityZ(1);
+ 
+  //look of tap movement on this axes - 1 == on; 0 == off
+  accel.setTapDetectionOnX(0);
+  accel.setTapDetectionOnY(0);
+  accel.setTapDetectionOnZ(1);
+ 
+  //set values for what is a tap, and what is a double tap (0-255)
+  accel.setTapThreshold(50); //62.5mg per increment
+  accel.setTapDuration(15); //625us per increment
+  accel.setDoubleTapLatency(80); //1.25ms per increment
+  accel.setDoubleTapWindow(200); //1.25ms per increment
+ 
+  //set values for what is considered freefall (0-255)
+  accel.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  accel.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+ 
+  //setting all interrupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  accel.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interrupt actions - 1 == on; 0 == off  
+  accel.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  accel.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  accel.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  accel.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  accel.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+
+  Serial.println("Accelerometer initialized");
 }
 
 void stopTest() {
@@ -179,6 +232,12 @@ void loop() {
   else {
     Serial.println("ERROR: Controller not connected");
   }
+  printGyro();
+  Serial.print("\t\t\t");
+  
+  printAccel();
+  Serial.println();
+  
   delay(12);
 }
 
@@ -259,8 +318,6 @@ void leftTrigger() {
   }
 }
 
-
-
 void printGyro() {
   int16_t x, y, z;
   gyro.getXYZ(&x, &y, &z);
@@ -269,55 +326,7 @@ void printGyro() {
   Serial.print(" ");
   Serial.print(y);
   Serial.print(" ");
-  Serial.println(z);
-}
-
-void initAccelerometer() {
-  accel.powerOn();
-  //set activity/ inactivity thresholds (0-255)
-  accel.setActivityThreshold(75); //62.5mg per increment
-  accel.setInactivityThreshold(75); //62.5mg per increment
-  accel.setTimeInactivity(10); // how many seconds of no activity is inactive?
- 
-  //look of activity movement on this axes - 1 == on; 0 == off 
-  accel.setActivityX(1);
-  accel.setActivityY(1);
-  accel.setActivityZ(1);
- 
-  //look of inactivity movement on this axes - 1 == on; 0 == off
-  accel.setInactivityX(1);
-  accel.setInactivityY(1);
-  accel.setInactivityZ(1);
- 
-  //look of tap movement on this axes - 1 == on; 0 == off
-  accel.setTapDetectionOnX(0);
-  accel.setTapDetectionOnY(0);
-  accel.setTapDetectionOnZ(1);
- 
-  //set values for what is a tap, and what is a double tap (0-255)
-  accel.setTapThreshold(50); //62.5mg per increment
-  accel.setTapDuration(15); //625us per increment
-  accel.setDoubleTapLatency(80); //1.25ms per increment
-  accel.setDoubleTapWindow(200); //1.25ms per increment
- 
-  //set values for what is considered freefall (0-255)
-  accel.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
-  accel.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
- 
-  //setting all interrupts to take place on int pin 1
-  //I had issues with int pin 2, was unable to reset it
-  accel.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
-  accel.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
-  accel.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
-  accel.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
-  accel.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
- 
-  //register interrupt actions - 1 == on; 0 == off  
-  accel.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
-  accel.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
-  accel.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
-  accel.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
-  accel.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+  Serial.print(z);
 }
 
 void printAccel() {
@@ -328,5 +337,5 @@ void printAccel() {
   Serial.print("  y-accel: ");
   Serial.print(xyz[1]);
   Serial.print("  z-accel: ");
-  Serial.println("xyz[2]");
+  Serial.print(xyz[2]);
 }
