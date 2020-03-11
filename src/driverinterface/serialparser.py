61e50c6428cf36ee601f58ebda1fd0962c2f8bbf
sm49697 <stamaria49697@sas.edu.sg>
2020-03-11 10:22:27 +0800

    documentation

--- src/driverinterface/serialparser.py
+++ src/driverinterface/serialparser.py
@@ -1,6 +1,13 @@
 import serial
 
 class SerialParser:
+    """
+    SerialParser uses pyserial to pull lines from the serial.
+    During setup, it scrapes all the servos and motors and their pins and 
+    names, then returns it. After setup, it is used for parsing the 
+    output of the serial's motors and servos, and generating a list of 
+    the outputs.
+    """
     SERIAL_PORT = None
     SERIAL_BAUDRATE = None
     ser = None
@@ -11,6 +18,12 @@ class SerialParser:
         open_serial()
 
     def open_serial():
+        """
+        Uses pyserial to open a serial connection with the port and baudrate
+        taken from the config. During setup, it parses the motors and servos
+        into a list. Once setup is finished, the serial is ready to read.
+        Called during initialization.
+        """
         try:
             read_config()
             ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE)
@@ -21,6 +34,10 @@ class SerialParser:
             print("Could not initialize serial")
 
     def read_config(self):
+        """
+        The serial port and baudrate is stored in 'config.txt'. This method
+        is used to get the update the port and baudrate.
+        """
         try:
             config = open("config.txt", "r")
             SERIAL_PORT = config.readline().decode('utf-8')
@@ -29,20 +46,36 @@ class SerialParser:
             print("Could not read config")
 
     def add_motors_servos(self):
+        """
+        Parses each line into a list of raw information of each motor/servo.
+        Adds the information into motors_servos_dict.
+        """
         line = self.read_serial_line()
         while(line != "starting 7.5s stop test       ||"):
             a_servo = parse_setup_input(line)
             motors_servos_dict.append(a_servo)
 
     def get_motor_servos(self):
+        """
+        Returns the list of raw information of the motors/servos.
+        """
         return motors_servos_dict
 
     def complete_setup(self):
+        """
+        Waits for the setup to be completed. Once it is, marks the port as
+        ready to read.
+        """
         line = self.read_serial_line()
         while(line != "setup completed"):
             continue
 
     def parse_setup_input(line):
+        """
+        The setup follows the format of name, motor/servo, several words,
+        then the pin. This scrapes the necessary information and returns
+        it as a tuple.
+        """
         contents = line.split(" ")
         servo_name = contents[0]  # motor/servo name should be first in list
         a_servo = contents[1]     # motor/servo should be second in list
@@ -50,9 +83,16 @@ class SerialParser:
         return (servo_name, a_servo, a_pin)
         
     def read_serial_line(self):
+        """
+        The serial returns a line in utf-8 format, so it must be decoded
+        to become more readable.
+        """
         ser.readline().decode('utf-8')
         
     def close_serial(self):
+        """
+        Must close the serial when quitting the application.
+        """
         ser.close()
     
     def get_port(self):
@@ -62,6 +102,9 @@ class SerialParser:
         return SERIAL_BAUDRATE
 
     def is_ready_to_read(self):
+        """
+        Checks whether setup is finished or not.
+        """
         return ready_to_read
 
     def __str__(self):
